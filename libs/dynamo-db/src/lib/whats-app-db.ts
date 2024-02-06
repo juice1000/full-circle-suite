f1418a4093610483e8614e9681c8f6d85d8b7097
juice1000 <julienlook@gmx.de>
2024-02-07 02:38:14 +0800

    revert message limit back to 5

--- libs/dynamo-db/src/lib/whats-app-db.ts
+++ libs/dynamo-db/src/lib/whats-app-db.ts
@@ -14,7 +14,7 @@ export async function getMessages(
       TableName: 'full-circle-messages',
       KeyConditionExpression: 'userId = :value',
       ScanIndexForward: false,
-      Limit: limit || 0,
+      Limit: limit || 5,
       ExpressionAttributeValues: {
         ':value': userId, // Use the appropriate data type (S for String, N for Number, etc.)
       },
